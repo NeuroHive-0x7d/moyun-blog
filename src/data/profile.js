4426eb458adfcd157552f2bac52d2fb58d5f29b6
Liu <liuli@mail.infuniv.com>
2026-05-11 20:11:23 +0800

    feat: 提交配置文件并更新 gitignore
    - 移除 gitignore 中对 profile.js、about.js、文章和图片的忽略规则
    - 提交 team 配置和 about 页面内容
    - profile.example.js 添加 siteUrl 字段

--- src/data/profile.js
+++ src/data/profile.js
@@ -1,8 +1,8 @@
 export default {
   siteUrl: 'https://neurohive-0x7d.github.io/moyun-blog',
   pageTitle: '墨韵 · NeuroHive',
-  blogTitle: '墨韵',
-  copyright: 'NeuroHive',
+  blogTitle: "墨韵",
+  copyright: "NeuroHive",
   avatar: '/images/avatar/avatar.png',
   name: 'NeuroHive',
   signature: '墨韵 · NeuroHive 团队的技术博客。',
